7c941f2d313cb538c376f6de1e82a6349c24769f
andre trosky <andre@andre-mbp.bigpond>
2013-01-26 19:27:17 +1100

    Initial Commit

---
+++ person_demo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  person_demo
+//
+//  Created by andre trosky on 26/01/13.
+//  Copyright (c) 2013 andre trosky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
